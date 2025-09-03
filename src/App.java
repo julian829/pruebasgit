c5e8dbbcf72148804b972016342eefb7fe748a94
alejo <alejoindigo@gmail.com>
2025-09-03 15:54:13 -0500

    cambio 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! mensaje 1");
+        System.out.println("Hello, World! mensaje 1 mensaje 2");
     }
 }
